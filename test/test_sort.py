0f50dc2a0c85f2edf94399b483f28a03032bfed0
CuddlyWarmth <116100124+CuddlyWarmth@users.noreply.github.com>
2024-03-18 12:50:30 +0100

    Made datsets

--- test/test_sort.py
+++ test/test_sort.py
@@ -5,12 +5,26 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 ### end
 
 from programs.rasp.sort import sort
+from programs.rasp.reverse import reverse
+from programs.rasp.hist import histogram
+from programs.rasp.double_hist import double_histogram
+from programs.rasp.most_freq import most_freq
 from src.utils import data_utils
 
+# For the tasks reverse, histogram, double histogram, sort and most-freq the standard length of 8 sequences
+# We try sequences of 16, 32 and 64
 
-df =  data_utils.make_sort(vocab_size=10, dataset_size=10, min_length=5, max_length=7, seed=0)
-print(df["sent"])
-print(df["tags"])
+# Create dataset for sort
+sort16 =  data_utils.make_sort(vocab_size=10, dataset_size=10000, min_length=16, max_length=18, seed=0)
+print(sort16["sent"])
+print(sort16["tags"])
+
+# Create rest of datasets
+reverse16 = data_utils.make_reverse(vocab_size=10, dataset_size=10000, min_length=16, max_length=18, seed=0)
+histogram16 = data_utils.make_histogram(vocab_size=10, dataset_size=10000, min_length=16, max_length=18, seed=0)
+double_histogram16 = data_utils.make_double_histogram(vocab_size=10, dataset_size=10000, min_length=16, max_length=18, seed=0)  
+most_freq16 = data_utils.make_most_freq(vocab_size=10, dataset_size=10000, min_length=16, max_length=18, seed=0)    
+
 
 a = sort.run(["<s>", "3", "1", "4", "2", "4", "0", "</s>"])
 
