e5f0b4d6992a7d5515f74d09b146c1149813cd23
CuddlyWarmth <116100124+CuddlyWarmth@users.noreply.github.com>
2024-03-21 14:06:17 +0100

    Programmed function to calculate "sequence level" accuracy

--- test/test_sort.py
+++ test/test_sort.py
@@ -12,6 +12,7 @@ from programs.rasp.double_hist import double_hist
 from programs.rasp.most_freq import most_freq
 from src.utils import data_utils
 from output.rasp.sort.vocab8maxlen12.transformer_program.headsc4headsn4nlayers3cmlps2nmlps2.s0 import sort as sort8
+from output.rasp.sort.vocab8maxlen8dvar100.transformer_program.headsc4headsn4nlayers3cmlps2nmlps2.s0 import sort as length_sort
 
 # For the tasks reverse, histogram, double histogram, sort and most-freq the standard length of 8 sequences
 # We try sequences of 16, 32 and 64
@@ -26,10 +27,10 @@ sort16 =  data_utils.make_sort(vocab_size=8, dataset_size=12, min_length=1, max_
 #double_histogram16 = data_utils.make_double_hist(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)  
 #most_freq16 = data_utils.make_most_freq(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)    
 
-print(sort16["sent"])
-print(len(sort16["sent"][0]))
+#print(sort16["sent"])
+#print(len(sort16["sent"][0]))
 #print(sort.run(sort16["sent"][0]))
-print(sort8.run(sort16["sent"][0]))
+#print(sort8.run(sort16["sent"][0]))
 #a = sort.run(["<s>", "3", "1", "4", "2", "4", "0", "2", "4", "</s>"])
 #print(a)
 #def test_program(df, program):
@@ -41,17 +42,21 @@ print(sort8.run(sort16["sent"][0]))
 #test_program(sort16, sort)
 
 def test_program(program):
-    sequence_lengths = np.arange(10, 110, 10)
+    sequence_lengths = np.arange(10, 20, 10)
+    sequence_lengths = np.array([8])
     results = {}
     for c in sequence_lengths:
         print(f"Sequence length: {c}")
         df =  data_utils.make_sort(vocab_size=8, dataset_size=12, min_length=c, max_length=c+2, seed=0)
         same = []
         for i in range(len(df)):
-            print(f"sent: {df["sent"][i]}")
-            print(f"found: {program.run(df["sent"][i])}")
-            print(f"tag: {df["tag"][i]}")
-            same.append(program.run(df["sent"][i]) == df["tag"][i])
+            print(f'sent: {df["sent"][i][1:-1]}')
+            print(f'found: {program.run(df["sent"][i])[1:-1]}')
+            print(f'tags: {df["tags"][i][1:-1]}')
+            same.append(program.run(df["sent"][i][1:-2]) == df["tags"][i][1:-2])
         results[c] = same.count(True)/len(same)
+    return results
 
+results = test_program(length_sort)
+print(results)
 
