e2c44c416d4bf34ac0ba610c72ad0452f2065954
CuddlyWarmth <116100124+CuddlyWarmth@users.noreply.github.com>
2024-03-21 12:05:07 +0100

    Kldfjls

--- test/test_sort.py
+++ test/test_sort.py
@@ -15,25 +15,24 @@ from src.utils import data_utils
 # We try sequences of 16, 32 and 64
 
 # Create dataset for sort
-b = 8
-c = b+2
-sort16 =  data_utils.make_sort(vocab_size=c, dataset_size=10000, min_length=b, max_length=c, seed=0)
+c=20
+sort16 =  data_utils.make_sort(vocab_size=8, dataset_size=12, min_length=1, max_length=c, seed=0)
 
 # Create rest of datasets
-reverse16 = data_utils.make_reverse(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)
-histogram16 = data_utils.make_hist(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)
-double_histogram16 = data_utils.make_double_hist(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)  
-most_freq16 = data_utils.make_most_freq(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)    
-print(sort16["sent"][0])
-print(sort.run(sort16["sent"][0]))
+#reverse16 = data_utils.make_reverse(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)
+#histogram16 = data_utils.make_hist(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)
+#double_histogram16 = data_utils.make_double_hist(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)  
+#most_freq16 = data_utils.make_most_freq(vocab_size=8, dataset_size=10000, min_length=16, max_length=18, seed=0)    
+print(sort16["sent"])
+#print(sort.run(sort16["sent"][0]))
 
-a = sort.run(["<s>", "3", "1", "4", "2", "4", "0", "</s>"])
-
-def test_program(df, program):
-    for i in range(len(df)):
-        print(df["sent"][i])
-        print(df["tags"][i])
-        print(program.run(df["sent"][i]))
+#a = sort.run(["<s>", "3", "1", "4", "2", "4", "0", "2", "4", "</s>"])
+#print(a)
+#def test_program(df, program):
+#    for i in range(len(df)):
+#        print(df["sent"][i])
+#        print(df["tags"][i])
+#        print(program.run(df["sent"][i]))
 
 #test_program(sort16, sort)
 
